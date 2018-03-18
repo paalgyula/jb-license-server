98851398a1f27025d6b412126bb0fe9824403792
Paál Gyula <paalgyula@paalgyula.com>
2018-03-18 14:22:26 +0100

    Dashboard rework

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
-  selector: 'admin-dashboard',
-  templateUrl: 'dashboard.component.html',
-  styles: []
+    selector: 'admin-dashboard',
+    templateUrl: 'dashboard.component.html',
+    styles: []
 })
 export class DashboardComponent implements OnInit {
+    private hostname: string;
 
-  constructor() { }
+    constructor(@Inject(DOCUMENT) private document) {
+        this.hostname = document.location.protocol +'//'+ document.location.hostname;
+        if ( document.location.port !== 443 || document.local.port !== 80 )
+            this.hostname += `:${document.location.port}`;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
